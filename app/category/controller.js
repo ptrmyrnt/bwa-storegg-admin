9d3f098f8cd01ab0d18d7af5d0ae85194a9be688
ptrmyrnt <femiliapm29@gmail.com>
2024-06-27 06:27:20 +0700

    read category admin

--- app/category/controller.js
+++ app/category/controller.js
@@ -3,7 +3,9 @@ const Category = require("./model");
 module.exports = {
   index: async (req, res) => {
     try {
-      res.render("admin/category/view_category");
+      const categories = await Category.find();
+
+      res.render("admin/category/view_category", { categories });
     } catch (err) {
       console.log("🚀 ~ index: ~ err:", err);
     }
