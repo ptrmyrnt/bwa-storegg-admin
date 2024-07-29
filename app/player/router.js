020c6e246f42abde97119dcdaafac13508a9f569
ptrmyrnt <femiliapm29@gmail.com>
2024-07-29 11:55:25 +0700

    feat:api transaction

--- app/player/router.js
+++ app/player/router.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { landingPage, detailPage, category } = require("./controller");
+const { landingPage, detailPage, category, checkout } = require("./controller");
+const { isLoginPlayer } = require("../middleware/auth");
 const router = express.Router();
 
 router.get("/landing-page", landingPage);
 router.get("/:_id/detail", detailPage);
 router.get("/category", category);
+router.post("/checkout", isLoginPlayer, checkout);
 
 module.exports = router;
